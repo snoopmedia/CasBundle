a813effcb9a147f893d752b0d14ad368a850e8dc
Dirk Olbertz <dirk.olbertz@gmail.com>
2013-04-29 21:30:07 +0300

    Create composer.js

---
+++ composer.js
@@ -0,0 +1,9 @@
+{
+    "name": "snoopmedia/casbundle",
+    "version": "1.0.0"
+    "homepage": "https://github.com/snoopmedia/CasBundle",
+    "autoload": {
+        "psr-0": { "Sensio\\CasBundle": "" }
+    },
+    "target-dir": "Sensio/Bundle/CasBundle",
+}
